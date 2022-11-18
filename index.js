904afd292c8481d655d39d5f42b829f766606d10
ezivan <114955741+ezivan@users.noreply.github.com>
2022-11-18 11:00:05 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+((d) => {
+  const $form = d.querySelector(".form");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let width = $form.formWidth.value,
+      height = ($form.formHeight.value / 100) ** 2,
+      imc = (width / height).toFixed(2);
+    $form.imcValue.value = `IMC: ${imc}`;
+
+    const imcDX = (imc) => {
+      if (imc < 18.5) return "DX. Peso Insuficiente :(";
+      if (imc >= 18.5 && imc <= 24.9) return "DX. Peso Normal :)";
+      if (imc >= 25.0 && imc <= 29.9) return "DX. Con sobrepeso :(";
+      if (imc >= 30) return "DX. Con obesidad :(";
+    };
+
+    $form.imcDx.value = imcDX(imc);
+  });
+})(document);
